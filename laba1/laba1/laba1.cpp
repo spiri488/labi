a6dfa65d52c7e5d67fc098ff9b249d1233fb1c18
spiri488 <115546507+spiri488@users.noreply.github.com>
2022-10-11 22:30:33 +0300

    Add files via upload

---
+++ laba1/laba1/laba1.cpp
@@ -0,0 +1,42 @@
+﻿
+
+#include <iostream>
+#include <locale>
+#include <cmath>
+#include <iomanip>
+#define G 10
+#define P 3.14159
+
+using namespace std;
+
+float dlina(float V, float ugol)
+
+{ 
+    while (ugol > 180) {
+        cout << "Введите корректное значение угла: "; cin >> ugol;
+    }
+    return (pow(V, 2) * 2 * abs(sin(ugol * P / 180)) * abs(cos(ugol * P / 180))) / G;
+
+ 
+}
+
+
+void main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    float V, ugol;
+
+    cout << "Укажите начальную скорость: "; cin >> V;
+    cout << "Укажите угол к горизонту: "; cin >> ugol;
+    cout << "Дальность полета тела с начальной скоростью: " << V << " и углом к горизонту: " << ugol << " составит: " 
+        << fixed << setprecision(3) << dlina(V, ugol);
+
+    printf("\nУкажите начальную скорость: "); scanf_s("%f", &V);
+    printf("\nУкажите угол к горизонту : "); scanf_s("%f", &ugol);
+    printf("\nДальность полета тела с начальной скоростью: %f и углом к горизонту: %f  составит: %10.3f", V, ugol, dlina(V, ugol));
+    
+  
+}
+
+
