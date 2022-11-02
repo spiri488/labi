12174f87deb811fdb81c0d01be6c3d3be0f7b80f
spiri488 <115546507+spiri488@users.noreply.github.com>
2022-11-02 06:46:06 +0300

    Add files via upload

---
+++ laba3/laba3/laba3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <locale>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    float x;
+    setlocale(LC_ALL, "RUS");
+
+    cout << "Введите значение аргумента функции:";
+    cin >> x;
+
+    double Sum = log(4), eps = 0.0001f;
+    int n = 1;
+
+    while (true)
+    {
+        double q = (1 / pow(4, n)) * (pow(x, n) / n);
+        Sum -= q;
+        n++;
+
+        if (q < eps)
+            break;
+    }
+
+    
+        
+    
+    cout << "\nСумма ряда= " << Sum << " Значение ln(4 - x)=" << log(4 - x);
+}
